5fbddd237e201a10e7c7c45226fbecab907c1c7b
yangtiger2005 <yangtiger2005@163.com>
2025-04-15 15:26:10 +0800

    增大写入数据量后，数据库存不进去数据

--- connect_influxdb.py
+++ connect_influxdb.py
@@ -1,43 +1,49 @@
+import random
 import time
 from datetime import timezone, timedelta, datetime
 
 import influxdb_client
 from influxdb_client import Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.client.write_api import SYNCHRONOUS, ASYNCHRONOUS
 
 token = "d3VP9A48seA-pnvdr7O-q4GGYJ2sFS1xU5ZU2iLHmVGupzydHeNGff3tPCxiUVMIDH3XPowYf99S3fGgX33wBA=="
 org = "ada"
 url = "http://localhost:8086"
-bucket = "initdb"
-measurement = "measurement1"
-
-# 生成中国时区时间戳（UTC+8）
-tz_shanghai = timezone(timedelta(hours=8))
+bucket = "init1"
+measurement = "measurement"
 
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
-write_api = client.write_api(write_options=SYNCHRONOUS)
+write_api = client.write_api(write_options=ASYNCHRONOUS)
 points = []
-for value in range(5):
-    now_shanghai_ns = int(datetime.now(tz_shanghai).timestamp() * 1e9)  # 微秒级时间戳乘以1e6 到纳秒级就乘以1e9
-    print(now_shanghai_ns)
-    print(int(datetime.now().timestamp() * 1e9))
-    point = Point(measurement).tag("sensor", "sensor1").field("temperature", value).time(now_shanghai_ns,
-                                                                                         write_precision=WritePrecision.NS)
-
+# now_ns = int((datetime.now() - timedelta(minutes=10)).timestamp() * 1e9)  # 微秒级时间戳乘以1e6 到纳秒级就乘以1e9
+for value in range(10):
+    print(value)
+    now_ns = int((datetime.now() - timedelta(minutes=6, microseconds=1)).timestamp() * 1e9)  # 微秒级时间戳乘以1e6 到纳秒级就乘以1e9
+    # point = Point(measurement).tag("sensor", "sensor1").field("temperature", value).time(
+    #     now_ns, write_precision=WritePrecision.NS)
+    temp = random.randint(0, 10)
+    point = {
+        "measurement": measurement,
+        "tags": {"sensor": "2号传感器"},
+        "fields": {"temperature": temp},
+        "time": now_ns
+    }
+    print(point)
     points.append(point)
-print(points)
+# print(points)
 try:
-    write_api.write(bucket=bucket, org="ada", record=points)
+    write_api.write(bucket=bucket, org="ada", record=points, write_precision=WritePrecision.NS)
 except Exception as e:
     print(e)
-time.sleep(1)  # separate points by 1 second
+# time.sleep(1)  # separate points by 1 second
+write_api.close()
 
-query_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+# query_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 query_api = client.query_api()
 
 query = f"""from(bucket: "{bucket}")
- |> range(start: -1d)
+ |> range(start: -7m)
  |> filter(fn: (r) => r._measurement == "{measurement}")
  """
 
@@ -45,6 +51,7 @@ print(query)
 tables = query_api.query(query=query, org=org)
 
 for table in tables:
-    for t in table.records:
-        print(t)
+    print(len(table.records))
+    # for t in table.records:
+    #     print(t)
 client.close()
